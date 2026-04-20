bd9b05489891447a59931b3b25f63ef0caef4793
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-20 13:07:29 +0100

    exemple two way et start rotating card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Color } from "./components/color/color";
+import { Two } from "./components/two/two";
+import { RotatingCard } from "./components/rotating-card/rotating-card";
 // métadonnée un décorateur qui informe le compilateur
 // que cette classe c'est un composant
 @Component({
   // Le sélécteur css qui va identifier le composant
   selector: 'app-root',
   // J'importe les dépendances de mon template
-  imports: [Color],
+  imports: [Color, Two, RotatingCard],
   // le lien vers le HTML que je gére
   templateUrl: './app.html',
   // Le css associé à ce composant
