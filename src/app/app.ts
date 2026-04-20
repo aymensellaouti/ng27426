d7584b9de803f855a8691a2c1ac1711f6844f5a8
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-20 09:41:47 +0100

    mise en place de bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+// métadonnée un décorateur qui informe le compilateur
+// que cette classe c'est un composant
 @Component({
+  // Le sélécteur css qui va identifier le composant
   selector: 'app-root',
+  // J'importe les dépendances de mon template
   imports: [RouterOutlet],
+  // le lien vers le HTML que je gére
   templateUrl: './app.html',
+  // Le css associé à ce composant
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ng27426');
+
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+// Bootstrap démarre l'application
+// il a besoin d'un truc qui s'appelle App (ca a l'air d'tre l'application !!)
+// et d'un truc qui s'appelle appConfig ca doit etre la config de l'application
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
