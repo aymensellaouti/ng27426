9d04f8d171603fa08ed83aec3ec383fc4b6efb01
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-20 14:45:52 +0100

    exemple effect

--- src/app/signals/ttc/ttc.component.ts
+++ src/app/signals/ttc/ttc.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-ttc',
   templateUrl: './ttc.component.html',
-  styleUrl: './ttc.component.css'
+  styleUrl: './ttc.component.css',
+  imports: [FormsModule]
 })
 export class TtcComponent {
-
+  // State
+  priceHt = signal(0);
+  taxe = signal(18);
+  qty = signal(1);
+  #errorEffect = effect(() => {
+    if (this.totalTtc() > 800 && this.totalTtc() <1000) {
+    console.info('Attention vous allez bientot dépasser votre solde seuil');
+    } else if (this.totalTtc() >= 1000) {
+      console.error('ATTENTION: Vous allez expolser votre budget, sortez vite courrez !!!!')
+    }
+  })
+  // à vérifier
+  unitaireTtc = computed(() => (this.priceHt() * (100 + this.taxe())) / 100);
+  discount = computed(() => {
+    let discountPercent = 0;
+    if (this.qty() >= 10 && this.qty() <= 15) {
+      discountPercent = 20;
+    } else if (this.qty() > 15) {
+      discountPercent = 30;
+    }
+    return (this.unitaireTtc() * discountPercent * this.qty()) / 100;
+  });
+  totalTtc = computed(() => (this.unitaireTtc() * this.qty()) - this.discount());
 }
