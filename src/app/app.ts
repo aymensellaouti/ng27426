66f33d25feaa72afc96d008ccb42a9c38b77281f
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-20 14:15:26 +0100

    start TTC exo

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,13 +4,14 @@ import { Two } from "./components/two/two";
 import { RotatingCard } from "./components/rotating-card/rotating-card";
 import { FirstSignal } from "./signals/first-signal/first-signal";
 import { Som } from "./signals/som/som";
+import { TtcComponent } from "./signals/ttc/ttc.component";
 // métadonnée un décorateur qui informe le compilateur
 // que cette classe c'est un composant
 @Component({
   // Le sélécteur css qui va identifier le composant
   selector: 'app-root',
   // J'importe les dépendances de mon template
-  imports: [Color, Two, RotatingCard, FirstSignal, Som],
+  imports: [Color, Two, RotatingCard, FirstSignal, Som, TtcComponent],
   // le lien vers le HTML que je gére
   templateUrl: './app.html',
   // Le css associé à ce composant
