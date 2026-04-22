cd14c6632e7b02e283fc11982553ddc25220749e
aymensellaouti <aymen.sellaouti@gmail.com>
2026-04-22 08:37:07 +0100

    mise en place du système de routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { First } from './components/first/first';
+import { CvPage } from './cv/cv-page/cv-page';
+import { MiniWordComponent } from './directives/mini-word/mini-word.component';
+import { Color } from './components/color/color';
+import { TodoComponent } from './todo/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: First},
+  { path: 'cv', component: CvPage},
+  { path: 'todo', component: TodoComponent},
+  { path: 'word', component: MiniWordComponent},
+  { path: 'color', component: Color},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
-import { Color } from "./components/color/color";
-import { Two } from "./components/two/two";
-import { RotatingCard } from "./components/rotating-card/rotating-card";
-import { FirstSignal } from "./signals/first-signal/first-signal";
-import { Som } from "./signals/som/som";
-import { TtcComponent } from "./signals/ttc/ttc.component";
-import { Pere } from "./commInterCompo/pere/pere";
-import { CvPage } from "./cv/cv-page/cv-page";
-import { Ngstyle } from "./directives/ngstyle/ngstyle";
-import { First } from "./components/first/first";
-import { Second } from "./components/second/second";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { Ngclass } from "./directives/ngclass/ngclass";
-import { TodoComponent } from "./todo/todo/todo.component";
-import { WeekTodoComponent } from "./todo/week-todo/week-todo.component";
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+
 // métadonnée un décorateur qui informe le compilateur
 // que cette classe c'est un composant
 @Component({
   // Le sélécteur css qui va identifier le composant
   selector: 'app-root',
   // J'importe les dépendances de mon template
-  imports: [Color, Two, RotatingCard, FirstSignal, Som, TtcComponent, Pere, CvPage, Ngstyle, First, Second, MiniWordComponent, Ngclass, TodoComponent, WeekTodoComponent],
+  imports: [RouterOutlet, Navbar],
   // le lien vers le HTML que je gére
   templateUrl: './app.html',
   // Le css associé à ce composant
